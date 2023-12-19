ad71cf28108df39e8251188b6d56cf5e6dd7743b
alereyes31 <alejandroreyes31.ar@gmail.com>
2023-12-18 19:36:01 -0500

    creacion de modelos y sus relaciones, controlador resource y de la tabla

---
+++ app/Http/Controllers/ResourceController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Illuminate\Support\Facades\Route;
+use App\Models\Resource;
+
+class ResourceController extends Controller
+{
+    public function index(Request $request)
+    {
+        return Inertia::render('Resources', [
+            'canLogin' => Route::has('login'),
+            'canRegister' => Route::has('register'),
+            'resources' => Resource::with('category')->get(),
+        ]);
+    }
+}
